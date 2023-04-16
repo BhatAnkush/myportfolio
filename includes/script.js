68c95e61d11b8ff54ebb3113571d58d1f03230b3
Ankush  Bhat <96989184+BhatAnkush@users.noreply.github.com>
2023-04-16 10:59:58 +0530

    Update script.js

--- includes/script.js
+++ includes/script.js
@@ -7,3 +7,7 @@ readMoreLink.onclick = function() {
     readMoreLink.style.display = "none";
   }
 };
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  alert("Please open this website on your laptop or desktop");
+}
